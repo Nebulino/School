14eb63cd78bff947e69d6ff5c4df5452ae060770
ale183 <ale183.exg@gmail.com>
2019-10-16 16:31:55 +0200

    Aggiunto un menu di scelta, aggiunto un controllo durante il prelievo per evitare un saldo negativo

--- Informatica/Java/Banca/src/core/Conto.java
+++ Informatica/Java/Banca/src/core/Conto.java
@@ -31,6 +31,10 @@ public class Conto {
             System.out.println("L'importo non può essere negativo");
             return;
         }
+        else if(importo > this.saldo){
+            System.out.println(String.format("Saldo non sufficiente (%s)", saldo));
+            return;
+        }
         this.saldo -= importo;
         addMovimento(-importo);
     }
@@ -49,12 +53,12 @@ public class Conto {
     }
 
     public String toString(){
-        String movimenti = "";
+        StringBuilder movimenti = new StringBuilder();
         for(int i = 0; i < this.movimenti.length; i++){
-            movimenti += String.format("Movimento %s: %s\n", i, this.movimenti[i]);
+            movimenti.append(String.format("Movimento %s: %s\n", i, this.movimenti[i]));
         }
         return(String.format("Iban: %s\n" +
                 "Saldo: %s\n" +
-                "%s", iban, saldo, movimenti));
+                "%s", iban, saldo, movimenti.toString()));
     }
 }
--- Informatica/Java/Banca/src/core/Main.java
+++ Informatica/Java/Banca/src/core/Main.java
@@ -1,14 +1,30 @@
 package core;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Conto uno = new Conto("ibanProva", 500);
-        System.out.println(uno.getSaldo());
-        uno.versamento(20);
-        uno.prelievo(70);
-        System.out.println(uno.getSaldo());
-        System.out.println(uno);
-        uno.prelievo(-100);
-        System.out.println(uno.getSaldo());
+        Conto conto = new Conto("IbanProva");
+        int i = -1;
+        Scanner scanner = new Scanner(System.in);
+        while(i != 0){
+            System.out.println("1. Versamento\n2. Prelievo\n3. Resoconto\n\n0. Esci");
+            i = scanner.nextInt();
+            switch (i){
+                case 1:
+                    System.out.println("Inserisci la cifra da versare: ");
+                    conto.versamento(scanner.nextInt());
+                    break;
+                case 2:
+                    System.out.println("Inserisci la cifra da prelevare: ");
+                    conto.prelievo(scanner.nextInt());
+                    break;
+                case 3:
+                    System.out.println(conto);
+                    break;
+                default:
+                    System.out.println("Valore non valido");
+            }
+        }
     }
 }
