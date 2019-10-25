65b3a1a368f20a083dfcb1f9b1d761686ed5c5e2
ale183 <ale183.exg@gmail.com>
2019-10-25 12:49:21 +0200

    Aggiunto setVolume senza specificare la posizione

--- Informatica/Java/2. Array Oggetti/src/core/Main.java
+++ Informatica/Java/2. Array Oggetti/src/core/Main.java
@@ -5,11 +5,17 @@ public class Main {
         Mensola mensola = new Mensola();
         Libro l1 = new Libro("Informatical", "Meini - Formichi", 180);
         Libro l2 = new Libro("Informatical2", "Meini - Formichi", 180);
-        if(mensola.setVolume(l1, 5) > -1){
-            System.out.println("Libro inserito correttamente");
-        }
-        else{
-            System.out.println("Posizione già occupata");
+        mensola.setVolume(l1, 17);
+        mensola.setVolume(l1, 18);
+        mensola.setVolume(l1, 19);
+        for(int i = 0; i < 50; i++){
+            int j = mensola.setVolume(l1);
+            if(j >= 0){
+                System.out.println(String.format("Libro messo nella posizione %s", j));
+            }
+            else {
+                System.out.println("Posizione occupata");
+            }
         }
     }
 }
--- Informatica/Java/2. Array Oggetti/src/core/Mensola.java
+++ Informatica/Java/2. Array Oggetti/src/core/Mensola.java
@@ -3,13 +3,16 @@ package core;
 public class Mensola {
     private final int DIM = 50;
     private Libro[] volumi;
+    private int dim;
 
     public Mensola(){
         volumi = new Libro[DIM];
+        dim = DIM;
     }
 
     public Mensola(int dim){
         volumi = new Libro[dim];
+        this.dim = dim;
     }
 
     public Mensola(Libro[] volumi){
@@ -22,5 +25,16 @@ public class Mensola {
         }
         this.volumi[posizione] = volume;
         return posizione;
+    }
+
+    public int setVolume(Libro volume){
+        int i;
+        for(i = 0; i < this.dim; i++){
+            if(this.volumi[i] == null) {
+                this.volumi[i] = volume;
+                return i;
+            }
+        }
+        return -1;
     }
 }
